4468231319a826fbfe39a069b9929445d6f0ea31
Xiaoran-Gao <xiaoran_gao@foxmail.com>
2026-08-23 13:06:40 +0800

    add solver and complete set1/c6.py

--- set1/c6.py
+++ set1/c6.py
@@ -1,17 +1,60 @@
+from binascii import hexlify, unhexlify
 from base64 import b64decode, b64encode
+from itertools import combinations
+from c3 import *
 
-def hamming_dist(str1, str2):
+def hamming_dist(b1, b2):
     """Compute the Hamming distance, which is the number of differing bits.
 
-    >>> test = hamming_dist("this is a test", "wokka wokka!!!")
-    >>> test
+    >>> hamming_dist(b"this is a test", b"wokka wokka!!!")
     37
     """
 
     dist = 0
-    for i, j in zip(bytes(str1, encoding='utf-8'), bytes(str2, encoding='utf-8')):
-        dist += (i ^ j)
+
+    bin1 = ''.join(format(i, '08b') for i in b1)
+    bin2 = ''.join(format(i, '08b') for i in b2)
+
+    for i, j in zip(bin1, bin2):
+        if i != j:
+            dist += 1
+
     return dist
 
+def best_keysize(ciphertext, lower_bound, upper_bound):
+    """Determine the keysize with the smallest normalized edit distance."""
+
+    keysize_dic = {i: None for i in range(lower_bound, upper_bound + 1)}
+
+    for k in list(keysize_dic.keys()):
+        blocks = [
+            ciphertext[: k],
+            ciphertext[k: 2*k],
+            ciphertext[2*k: 3*k],
+            ciphertext[3*k: 4*k]
+        ]
+        dist = []
+        for block1, block2 in combinations(blocks, 2):
+            dist.append(hamming_dist(block1, block2) / k)
+
+        keysize_dic[k] = sum(dist) / len(dist)
+    
+    keysize_lis_asc = sorted(keysize_dic, key=keysize_dic.get)
+
+    return keysize_lis_asc[0]
+
+def solver(keysize, ciphertext):
+    """Find the key, and break the ciphered text."""
+
+    # Break the text into blocks and transpose
+    t_blocks = [ciphertext[i::keysize] for i in range(keysize)]
+    t_blocks_hex = [hexlify(b) for b in t_blocks]
+    key = [xor_decipher(h)[0] for h in t_blocks_hex]
+    
+    return key
+
 if __name__ == "__main__":
-    print(hamming_dist("this is a test", "wokka wokka!!!"))
+    b64_text = open('./set1/input/6.txt', 'r').read()
+    text = b64decode(b64_text)
+    keysize = best_keysize(text, 2, 40)
+    print(''.join(solver(keysize, text)))
